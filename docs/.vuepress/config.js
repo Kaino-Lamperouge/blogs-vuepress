8f83a318c7c66f71b3cc35288fa8648a53909edd
muchen0905 <1832612650@qq.com>
2022-04-18 10:53:20 +0800

    uuid

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,50 +1,90 @@
 const { getMdByDir } = require("./utils");
 
 module.exports = {
-  title: "muchen6",
+  title: "zhoumuchen",
   // description: "This is a blog.",
   base: '/blogs/',
   serviceWorker: true, // 阅读缓存进程
   theme: '@vuepress/theme-default',
   themeConfig: {
-    navbar: [{
-      text: 'Home',
-      link: '/'
-    },
-    {
-      text: 'Tools',
-      // children: getMdByDir("tools/", "tools")
-      children: ["/tools/markdown_grammar.md", "/tools/shell_introduce.md", "/tools/timestamp_introduce.md", "/tools/JavaScript/export.md", "/tools/JavaScript/object.md", "/tools/git.md", "/tools/npm.md", "/tools/优化.md"]
-      // children: [
-      //   // {
-      //   //     text: "Markdown",
-      //   //   //   chilren: getMdByDir("tools/markdown", "tools/markdown")
-      //   //     children: ["/tools/markdown/markdown_grammar.md"]
-      //   // }
-      //   {
-      //     text: "Markdown",
-      //     // link: "/tools/markdown_grammar.html"
-      //     children: ["/tools/markdown_grammar.md"]
-      //   },
-      //   {
-      //     text: "Shell",
-      //     // link: "/tools/shell_introduce.html"
-      //     children: ["/tools/shell_introduce.md"]
-      //   }
-      // ]
-    },
-    {
-      text: 'About',
-      link: '/about/'
-    },
+    navbar: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'Tools',
+        // children: getMdByDir("tools/", "tools")
+        children: [
+          "/tools/markdown.md",
+          "/tools/shell.md",
+          "/tools/timestamp.md",
+          "/tools/JavaScript/export.md",
+          "/tools/JavaScript/object.md",
+          "/tools/git.md",
+          "/tools/npm.md",
+          "/tools/优化.md",
+        ]
+        // children: [
+        //   // {
+        //   //     text: "Markdown",
+        //   //   //   chilren: getMdByDir("tools/markdown", "tools/markdown")
+        //   //     children: ["/tools/markdown/markdown_grammar.md"]
+        //   // }
+        //   {
+        //     text: "Markdown",
+        //     // link: "/tools/markdown_grammar.html"
+        //     children: ["/tools/markdown_grammar.md"]
+        //   },
+        //   {
+        //     text: "Shell",
+        //     // link: "/tools/shell_introduce.html"
+        //     children: ["/tools/shell_introduce.md"]
+        //   }
+        // ]
+      },
+      {
+        text: '金晨',
+        children: [
+          "/chen/film.md",
+          "/chen/flight.md",
+          "/chen/stage.md",
+          "/chen/TV play.md",
+          "/chen/variety.md",
+        ]
+      },
+      { text: 'About', link: '/about/' },
     ],
     sidebar: {
-      '/tools/': [{
-        text: "Tools",
-        collapsible: true,
-        // children: getMdByDir("tools/", "tools")
-        children: ["/tools/markdown_grammar.md", "/tools/shell_introduce.md", "/tools/timestamp_introduce.md", "/tools/JavaScript/export.md", "/tools/JavaScript/object.md", "/tools/git.md", "/tools/npm.md", "/tools/优化.md"]
-      }]
+      '/tools/': [
+        {
+          text: "Tools",
+          collapsible: true,
+          // children: getMdByDir("tools/", "tools")
+          children: [
+            "/tools/markdown.md",
+            "/tools/shell.md",
+            "/tools/timestamp.md",
+            "/tools/JavaScript/export.md",
+            "/tools/JavaScript/object.md",
+            "/tools/git.md",
+            "/tools/npm.md",
+            "/tools/优化.md",
+          ]
+        }
+      ],
+      '/chen/': [
+        {
+          text: "金晨",
+          collapsible: true,
+          // children: getMdByDir("tools/", "tools")
+          children: [
+            "/chen/film.md",
+            "/chen/flight.md",
+            "/chen/saying.md",
+            "/chen/stage.md",
+            "/chen/TV play.md",
+            "/chen/variety.md",
+          ]
+        }
+      ]
     },
     search: true,
     searchMaxSuggestions: 10
